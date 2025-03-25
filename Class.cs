9f3ff6dace9243cc90d1570ee35803b147a9e8e0
Fascinate <eddo92@yahoo.it>
2025-03-25 16:47:41 +0100

    Update Class.cs

--- Class.cs
+++ Class.cs
@@ -1,167 +1,158 @@
-namespace Pokemon;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Threading.Channels;
 
-class Pokemon
+namespace Coding.Exercise
 {
-    public  string nome ;
-    public string tipo ;
-    public string tipo2;
-    public int hp ;
-    
-    public Pokemon(string nome, string tipo, int hp)
+    class Pokemon
     {
-     this.nome = nome;
-     this.tipo = tipo; 
-     this.hp = hp;
-    }
-    
-    public Pokemon(string nome, string tipo,string tipo2, int hp)
-    {
-        this.nome = nome;
-        this.tipo = tipo; 
-        this.tipo2 = tipo2;
-        this.hp = hp;
-    }
-    
+        public string Name { get; }
+        public int Hp { get; private set; }
+        public string Type { get; }
+        public string Type2 { get; }
+        public string TipoMossa { get; private set; }
+        public static int counter = 0;
+        private static Random rng = new Random();
+        
 
-    public int Fulmine(Pokemon opponent)
-    {
-        Console.WriteLine($"{nome} lancia fulmine");
-        string tipoMoves = "Elettro";
-        int damage = 70;
-        if (opponent.tipo == "Acqua" || opponent.tipo2 == "Acqua")
+        public Pokemon(string name,int hp,string type)
         {
-            damage = damage * 2;
-            
-        }
-        else if (opponent.tipo == "Terra"|| opponent.tipo2 == "Terra")
-        {
-            damage = damage * 0;
-            Console.WriteLine("Non ha effetto");
-        }
-        opponent.hp -= damage;
-        StatBattaglia(opponent, damage, opponent.hp);
-
-        opponent.hp -= damage;
-        return  opponent.hp ;
-    }
-
-    public int Semitraglia(Pokemon opponent)
-    {
-        Console.WriteLine($"{nome} lancia semitraglia");
-        Random tiri = new Random();
-        string tipoMoves = "Erba";
-        int BaseDamage = 15;
-        int damage = 0;
-        int counter = 0;
-        int lancio = tiri.Next(1, 10); Console.WriteLine($"lancio {counter}");
-        if (opponent.tipo == "Roccia" || opponent.tipo2 == "Terra")
-        {
-            BaseDamage *=2;
-
-
-        }
-        do
-        {
+            this.Name = name;
+            this.Hp = hp;
+            this.Type = type;
             counter++;
-            lancio = tiri.Next(1, 10);
-            Console.WriteLine($"lancio {counter}");
+        }
+        public Pokemon(string name,int hp,string type,string type2)
+        {
+            this.Name = name;
+            this.Hp = hp;
+            this.Type = type;
+            this.Type2 = type2;
+            counter++;
+        }
+        public int Attacco(Pokemon opponent, string nomeMossa, int damage, string tipoMossa)
+{
+    Console.WriteLine($"{Name} usa {nomeMossa} su {opponent.Name}");
 
-            damage += BaseDamage;
-            opponent.hp -= damage;
-
-        } while (counter <4 && lancio >= 4 );
-        opponent.hp -= damage;
-        StatBattaglia(opponent, damage, opponent.hp);
-        return opponent.hp;
+    // Verifica superefficacia
+    if ((tipoMossa == "Elettro" && (opponent.Type == "Volante" || opponent.Type2 == "Volante" ||
+                                    opponent.Type == "Acqua" || opponent.Type2 == "Acqua")) ||
+        (tipoMossa == "Erba" && (opponent.Type == "Acqua" || opponent.Type2 == "Acqua" ||
+                                 opponent.Type == "Terra" || opponent.Type2 == "Terra" ||
+                                 opponent.Type == "Roccia" || opponent.Type2 == "Roccia")) ||
+        (tipoMossa == "Drago" && (opponent.Type == "Drago" || opponent.Type2 == "Drago")) ||
+        (tipoMossa == "Volante" && (opponent.Type == "Erba" || opponent.Type2 == "Erba")) ||
+        (tipoMossa == "Fuoco" && (opponent.Type == "Erba" || opponent.Type2 == "Erba" ||
+                                  opponent.Type == "Acciaio" || opponent.Type2 == "Acciaio")) ||
+        (tipoMossa == "Acqua" && (opponent.Type == "Roccia" || opponent.Type2 == "Roccia" ||
+                                  opponent.Type == "Terra" || opponent.Type2 == "Terra")) ||
+        (tipoMossa == "Acciaio" && (opponent.Type == "Roccia" || opponent.Type2 == "Roccia")) ||
+        (tipoMossa == "Roccia" && (opponent.Type == "Volante" || opponent.Type2 == "Volante" ||
+                                   opponent.Type == "Fuoco" || opponent.Type2 == "Fuoco")))
+    {
+        damage *= 2;
+        Console.WriteLine("È superefficace!");
+    }
+    // Verifica inefficacia
+    else if ((tipoMossa == "Elettro" && (opponent.Type == "Elettro" || opponent.Type2 == "Elettro")) ||
+             (tipoMossa == "Normale" && (opponent.Type == "Roccia" || opponent.Type2 == "Roccia" ||
+                                         opponent.Type == "Acciaio" || opponent.Type2 == "Acciaio")) ||
+             (tipoMossa == "Volante" && (opponent.Type == "Acciaio" || opponent.Type2 == "Acciaio" ||
+                                         opponent.Type == "Elettro" || opponent.Type2 == "Elettro" ||
+                                         opponent.Type == "Roccia" || opponent.Type2 == "Roccia")) ||
+             (tipoMossa == "Fuoco" && (opponent.Type == "Fuoco" || opponent.Type2 == "Fuoco" ||
+                                       opponent.Type == "Acqua" || opponent.Type2 == "Acqua" ||
+                                       opponent.Type == "Roccia" || opponent.Type2 == "Roccia" ||
+                                       opponent.Type == "Drago" || opponent.Type2 == "Drago")) ||
+             (tipoMossa == "Erba" && (opponent.Type == "Volante" || opponent.Type2 == "Volante")) ||
+             (tipoMossa == "Roccia" && (opponent.Type == "Acciaio" || opponent.Type2 == "Acciaio" ||
+                                        opponent.Type == "Terra" || opponent.Type2 == "Terra")))
+    {
+        damage /= 2;
+        Console.WriteLine("Non è molto efficace...");
+    }
+    // Verifica nessun effetto
+    else if ((tipoMossa == "Elettro" && (opponent.Type == "Terra" || opponent.Type2 == "Terra")) ||
+             (tipoMossa == "Normale" && (opponent.Type == "Spettro" || opponent.Type2 == "Spettro")))
+    {
+        damage = 0;
+        Console.WriteLine($"Non ha effetto su {opponent.Name}!");
     }
 
-    public int Codacciaio(Pokemon opponent)
-    {
-        Console.WriteLine($"{nome} usa codacciaio");
-        string tipoMoves = "Acciaio";
-        int damage = 70;
-        
+    // Applica il danno
+    opponent.Hp -= damage;
+    int HpRimasti = opponent.Hp;
 
-        if (opponent.tipo == "Roccia")
-        {
-            damage = damage * 2;
-            
-        }
-        
-        opponent.hp -= damage;
-        StatBattaglia(opponent, damage, opponent.hp);
+    Console.WriteLine($"{Name} infligge {damage} danni a {opponent.Name}!");
 
-        return opponent.hp;
-    }
+    // Controlla se l'avversario è K.O.
+    CheckDeathOpponent(opponent, HpRimasti);
+    
 
-    public int FoglieLama(Pokemon opponent)
-    {
-        int damage = 50;
-        string tipoMoves = "Erba";
-        Console.WriteLine($"{nome} usa foglieLama");
-
-        if (opponent.tipo == "Roccia" || opponent.tipo2 == "Terra")
-        {
-            damage = damage * 2;
-        }
-        opponent.hp -= damage;
-        StatBattaglia(opponent,damage,opponent.hp);
-        return opponent.hp;
-    }
-
-    public int Sassata(Pokemon opponent)
-    {
-        int damage = 70;
-        string tipoMoves = "Roccia";
-        Console.WriteLine($"{nome} usa Sassata");
-        
-        opponent.hp -= damage;
-        StatBattaglia(opponent, damage, opponent.hp);
-        return opponent.hp;
-        
-    }
-
-    public int BasicAttackWater(Pokemon opponent, int danni,string tipoMossa,string nomeMossa )
-    {
-        int damage = danni;
-        string tipoMoves = tipoMossa;
-        Console.WriteLine($"{nome} usa {nomeMossa}");
-        if (opponent.tipo == "Roccia" || opponent.tipo2 == "Terra")
-        {
-            damage = damage * 2;
-        }
-        opponent.hp -= damage;
-        StatBattaglia(opponent, damage, opponent.hp);
-        return opponent.hp;
-    }
-
-    public int BasicAttackNormal(Pokemon opponent, int danni, string tipoMossa, string nomeMossa)
-    {
-        int damage = danni;
-        string tipoMoves = tipoMossa;
-        Console.WriteLine($"{nome} usa {nomeMossa}");
-        if (opponent.tipo == "Roccia" || opponent.tipo2 == "Acciaio")
-        {
-            damage = damage / 2;
-            Console.WriteLine("non è molto efficace");
-        }
-
-        StatBattaglia(opponent, damage, opponent.hp);
-        opponent.hp -= damage;
-        return opponent.hp;
-    }
-
-    public void StatBattaglia(Pokemon opponent, int damage, int totale)
-    {
-        if (opponent.hp> 0)
-        {
-            Console.WriteLine($"fa un danno di {damage} , {opponent.nome} ora ha {opponent.hp} Hp.");
-        }
-
-        else
-        {
-            Console.WriteLine($"fa un danno di {damage} , {opponent.nome} è KO");
-        }
-        
+    return HpRimasti;
 }
+
+        private bool Paralisi(Pokemon Opponent, string tipoMossa)
+        {
+            bool status = false;
+            if (tipoMossa == "Elettro" && Opponent.Type != "Terra" && (Opponent.Type2 == null || Opponent.Type2 != "Terra"))
+            {
+                
+                int paralisi = rng.Next(1,10);
+                if (paralisi >0 && paralisi <=3)
+                { 
+                    status = true;
+                    
+                }
+            }
+
+            return status;
+        }//Da applicare
+
+        private int UsaRandomMove(Pokemon opponent, string nomeMossa, int damage)
+        {
+            int danno = damage;
+            Console.WriteLine($"{Name} usa {nomeMossa} su {opponent.Name}");
+            int HpRimasti = opponent.Hp -= damage;
+            CheckDeathOpponent(opponent,HpRimasti);
+            return HpRimasti;
+        }
+        private void CheckDeathOpponent(Pokemon opponent,int hpRimasti)
+        {
+            if (hpRimasti > 0)
+            {
+                Console.WriteLine($"{opponent.Name} ha {opponent.Hp} punti Vita");
+            }
+            else
+            {
+                Console.WriteLine($"{opponent.Name} è KO");
+                
+            }
+            
+                
+        }
+
+        public int StrumentoCuraHp (Pokemon pkmDaCurare,int hpDaRecuperare,string nomeStrumento)
+        {
+            pkmDaCurare.Hp += hpDaRecuperare;
+            Console.WriteLine($"Usa {nomeStrumento} per recuperare {hpDaRecuperare}Hp\n{Name} ora ha {pkmDaCurare.Hp} hp");
+
+            int hpRimasti = pkmDaCurare.Hp;
+            
+            return hpRimasti;
+
+        }
+        
+
+        
+        
+        
+        
+    }
+ 
+    
+    
+    
+    
 }
